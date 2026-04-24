5f414d80bcd0454ae8bd860f5b2b089b2477a247
Ahmat issa Annadif <ahmatking72@gmail.com>
2026-04-24 21:03:05 +0300

    Ajout

---
+++ 7-2Hafta.js
@@ -0,0 +1,9 @@
+const job = "developer";
+
+let myNewJob;
+//Çözüm: İlk harfi al ve büyüt. Sonra geri kalan ifadeyi de sonuna ekle
+myNewJob = job.charAt(0).toUpperCase() + job.substring(1);
+//veya
+myNewJob = job[0].toUpperCase() + job.slice(1);
+
+console.log(myNewJob);
